b72d76ba42eb34630fb868493d5a2f9e629eaa41
Gabriel-Takeda <124248063+Gabriel-Takeda@users.noreply.github.com>
2025-07-03 22:20:41 -0300

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        System.out.println("Hello and welcome!")
         System.out.println(celsiusToFahrenheit(0));
         System.out.println(fahrenheitToCelsius(32));
 
